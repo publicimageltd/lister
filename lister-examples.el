1555b36379ab4ec4c1932cbc5be727011ef72897
Jörg Volbers <joerg@joergvolbers.de>
2020-06-11 14:56:50 +0200

    Edit .gitignore, update examples

--- lister-examples.el
+++ lister-examples.el
@@ -23,7 +23,7 @@
 ;; 
 
 ;;; Code:
-(load-file "../lister/lister.el")
+(load-file "lister.el")
 
 ;; -----------------------------------------------------------
 
@@ -41,34 +41,144 @@
   (message "Stored data: %s."
 	   (lister-get-data (current-buffer) :point)))
 
+(defun lister-filter-by-a (data)
+  (apply-partially #'string-match "\\`A"))
+
+(defun lister-filter-by-b (data)
+  (apply-partially #'string-match "\\`B"))
+
+(defun lister-key-filter-by-a ()
+  (interactive)
+  (lister-set-filter (current-buffer) #'lister-filter-by-a))
+
+(defun lister-key-filter-by-b ()
+  (interactive)
+  (lister-set-filter (current-buffer) #'lister-filter-by-b))
+
+(defun lister-key-toggle-highlight-mode ()
+  (interactive)
+  (call-interactively 'lister-highlight-mode))
+
+(defun lister-key-highlight-intangible ()
+  (interactive)
+  (save-excursion
+    (let* ((inhibit-read-only t)
+	   (cursor-sensor-inhibit t))
+      (goto-char (point-min))
+      (while (not (eobp))
+	(let ((plist (text-properties-at (point)))
+	      (next-change (or (next-property-change (point) (current-buffer))
+			       (point-max))))
+	  (when (plist-get plist 'cursor-intangible)
+	    (put-text-property (1+ (point)) next-change
+			       'face 'highlight))
+	  (goto-char next-change))))))
+    
 (defun lister-interactive-test ()
   (interactive)
   (message "Neuer Start von `lister-interactive-test'")
   (let* ((lister-buf (lister-setup
 		      (lister-test-buffer)
 		      #'lister-mapper
-		      '("A" "B" "C")
+		      '("AA" "AB" "BA" nil "BC")
 		      "HEADER"
 		      "FOOTER")))
     (lister-add-enter-callback lister-buf #'lister-item-message)
+    (define-key lister-mode-map "a" #'lister-key-filter-by-a)
+    (define-key lister-mode-map "b" #'lister-key-filter-by-b)
+    (define-key lister-mode-map "h" #'lister-key-toggle-highlight-mode)
+    (define-key lister-mode-map "i" #'lister-key-highlight-intangible)
     (switch-to-buffer lister-buf)
-    (lister-highlight-mode)))
+;;    (lister-highlight-mode)
+    ))
 
 ;; -----------------------------------------------------------
 ;; Narrowing
 
 (defun lister-narrow-test ()
   (interactive)
+  (setq lister-narrow-input nil) ;; muss in lister-setup rein
   (let* ((files     (seq-filter
 		     (apply-partially #'string-match "\\`[^.#%]")
 		     (directory-files user-emacs-directory)))
 	 (lister-buf (lister-setup
 		      (lister-test-buffer)
-		      #'list 
+		      #'lister-narrow-mapper
 		      files
 		      (format "%s:" user-emacs-directory))))
     (switch-to-buffer lister-buf)
-    (lister-highlight-mode)))
+    (lister-highlight-mode)
+    (lister-narrow-with-minibuffer files)
+    ))
+
+;; TODO Es sollte eine globale Variable "lister-narrow-minibuffer-state" geben;
+;; eine PLIST.
+
+;; TODO Für async einen Timer vor dem update einbauen (siehe ivy--quehe-exhibit)
+
+;; see ivy--minibuffer-setup
+(defun lister-narrow-interactive-minibuffer-setup ()
+  (add-hook 'post-command-hook #'lister-narrow-update nil t))
+
+(defun lister-narrow-string-empty-p (s)
+  "Return t if S is neither nil nor \"\"."
+  (or (null s)
+      (string-empty-p s)))
+
+(defun lister-narrow-highlight-regexp (s regexp face-or-plist)
+  "Highlight first match of REGEXP in S with FACE-OR-PLIST."
+  (with-temp-buffer
+    (insert  s)
+    (goto-char (point-min))
+    (ignore-errors
+      (when (re-search-forward regexp (point-max) t)
+	(lister-add-face-property (match-beginning 0) (match-end 0) face-or-plist)))
+    (buffer-string)))
+
+(defun lister-narrow-mapper (data)
+  "Print DATA as a string and highlight match."
+  (if (lister-narrow-string-empty-p lister-narrow-input)
+      data
+    (lister-narrow-highlight-regexp data
+				    lister-narrow-input
+				    'org-todo)))
+
+(defun lister-narrow-propertize-minibuffer-prompt (face-or-plist &optional de-propertize)
+  (with-current-buffer (window-buffer (minibuffer-window))
+    (let* ((inhibit-field-text-motion t)
+	   (inhibit-read-only t)
+	   (fn (if de-propertize 'lister-remove-face-property 'lister-add-face-property)))
+      (funcall fn (line-beginning-position) (minibuffer-prompt-end) face-or-plist))))
+
+(defun lister-narrow-match-p (regexp data) 
+  "Check if DATA is matched by REGEXP."
+  (condition-case err
+      (and (string-match-p regexp data)
+	   (lister-narrow-propertize-minibuffer-prompt 'org-todo t)
+	   t)
+    (error
+     (lister-narrow-propertize-minibuffer-prompt 'org-todo)
+     data)))
+
+(defun lister-narrow-update ()
+  (setq lister-narrow-input (minibuffer-contents-no-properties))
+  (lister-set-list lister-narrow-buffer
+		   (seq-filter (apply-partially
+				#'lister-narrow-match-p
+				lister-narrow-input)
+			       lister-narrow-list)))
+
+(defun lister-narrow-with-minibuffer (datalist)
+  (interactive)
+  (let* ((calling-buffer (window-buffer (minibuffer-selected-window))))
+    (unless (lister-buffer-p calling-buffer)
+      (user-error "This function has to be called from within a lister buffer."))
+    (setq lister-narrow-buffer calling-buffer)
+    (setq lister-narrow-list   datalist)
+    (setq lister-narrow-input  nil)
+    (minibuffer-with-setup-hook
+	#'lister-narrow-interactive-minibuffer-setup
+      (read-from-minibuffer "Test: "))))
 
 (provide 'lister-examples)
 ;;; lister-examples.el ends here
