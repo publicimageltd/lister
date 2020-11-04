066a62299f242a8652ae0a457ca4257fd81fd446
Jörg Volbers <joerg@joergvolbers.de>
2020-11-04 20:29:16 +0100

    Add keyword to satisfy linter.

--- lister-highlight.el
+++ lister-highlight.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2020  
 
 ;; Author:  <joerg@joergvolbers.de>
-;; Keywords: 
+;; Keywords: hypermedia
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
