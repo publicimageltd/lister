257b47ac9be6c6f0b0aea0ae18f3d62e27fb3262
Jörg Volbers <joerg@joergvolbers.de>
2021-11-06 21:14:30 +0100

    Remove outdated comment

--- lister.el
+++ lister.el
@@ -25,9 +25,6 @@
 
 ;; A library for creating interactive list buffers.
 
-;;; TODO When marking, allow hook to additionally check if item at
-;;; point is markable (~ to "marking predicate")
-
 ;;; Code:
 
 (require 'cl-lib)
